d1de061df9b94eee12a9ff404f184200bc1b73bb
Ant0CPU <antonio.bruno@marconibari.edu.it>
2025-03-01 13:27:09 +0000

    Consegna

--- main.cpp
+++ main.cpp
@@ -1,11 +1,45 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
 int main() {
-    string nome;
-    cout << "Inserisci il tuo nome: ";
-    cin >> nome;
-    cout << "Ciao " <<nome<< "!" << endl;
-}
+    int n, i;
+    float Svoti, media;
 
-//LEGGERE LE ISTRUZIONI NEL FILE README.md
+    do{
+        cout<<"Inserire il numero di studenti"<<endl;
+        cin>>n;
+    } while (n<=0);
+
+    vector<string>nomi(n);
+    vector<float>voti(n);
+    vector<int>crediti(n);
+
+    i=0;
+    while (i<n) {
+        cout<<"Inserire il nome dello studente"<<endl;
+        cin>>nomi[i];
+        
+        cout<<"Inserire la valutazione dello studente"<<endl;
+        cin>>voti[i];
+        
+        cout<<"Inserire il numero di crediti acquisiti dallo studente"<<endl;
+        cin>>crediti[i];
+
+        i=i+1;
+    }
+
+    Svoti=0;
+    for (i=0; i<n; i++) {
+        Svoti=Svoti+voti[i];
+    }
+    media=Svoti/n;
+
+    i=0;
+    while (i<n) {
+        if (voti[i]>media and crediti[i]>=6) {
+            cout<<"Lo studente "<<nomi[i]<<", ha conseguito una valutazione maggiore della media e dei crediti formativi maggiori di sei"<<endl;
+        }
+        i=i+1;
+    }
+}
